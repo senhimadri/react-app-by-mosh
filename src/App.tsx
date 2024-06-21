5cc98bd6947ceb5e0a26aac957616f557be75834
senhimadri <senhimadri.dev@gmail.com>
2024-06-21 12:28:59 +0600

    Add New Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 
 function App(){
     let items =['New York','San Francisco','Tokyo','London','Paris'];
-
     const onSelectHandler = (item : string)=>{
-
         console.log(item);
     }
 
-    return <div> <ListGroup items={items} heading="Cities" onSelectItem={onSelectHandler} /> </div>
+    return (
+        <>
+            <div> <ListGroup items={items} heading="Cities" onSelectItem={onSelectHandler} /> </div>
+            <Alert>
+              <b>Hello</b>   <span>World</span> 
+            </Alert>
+        </>
+    )
+    
+    
+    
 }
 
 export default App;
